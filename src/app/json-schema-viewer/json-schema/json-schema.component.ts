b7cc262e08e622953d7f83e7974b4813ce48962d
dav770 <47830985+dav770@users.noreply.github.com>
2020-02-02 09:58:24 +0200

    commit_before_merge

--- src/app/json-schema-viewer/json-schema/json-schema.component.ts
+++ src/app/json-schema-viewer/json-schema/json-schema.component.ts
@@ -18,9 +18,9 @@ export class JsonSchemaComponent implements OnInit {
     // @ts-ignore
     json                                      = require ( '../testJSON.json' );
     position_before: { x: number, y: number } = { x: 0, y: 0 };
-    
+
     position_after: { x: number, y: number } = { x: 0, y: 0 };
-    
+
     isDragged: boolean                      = false;
     deltaDragging: { x: number, y: number } = { x: 0, y: 0 };
     // @ts-ignore
@@ -32,17 +32,17 @@ export class JsonSchemaComponent implements OnInit {
     width                                   = 2500 - this.margin.right - this.margin.left;
     height                                  = 3000 - this.margin.top - this.margin.bottom;
     currentScale: number                    = 1;
-    
+
     constructor( private   jsonSchemaService: JsonSchemaService ) {
-    
+
     }
-    
+
     handleZoomPlus() {
         this.currentScale += 0.25;
         console.log ( '[c] zoomPlus', this.svg );
-    
+
         d3.select ( 'app-json-schema' ).call (
-            
+
             // @ts-ignore
             d3.behavior.zoom ()
             // .extent([[0, 0], [this.width, this.height]])
@@ -51,11 +51,11 @@ export class JsonSchemaComponent implements OnInit {
                 console.log ( '[c] zoom', d3.event );
                 // @ts-ignore
                 d3.select ( 'app-json-schema' ).attr ( 'transform', 'translate(' + d3.event.translate + ')scale(' + this.currentScale + ')' );
-                
+
             } ) );
-        
+
     }
-    
+
     handleZoomMinus() {
         console.log ( '[c] zoomMinus' );
         this.currentScale -= 0.25;
@@ -68,18 +68,18 @@ export class JsonSchemaComponent implements OnInit {
                 console.log ( '[c] zoom', d3.event );
                 // @ts-ignore
                 this.svg.attr ( 'transform', 'translate(' + d3.event.translate + ')scale(' + this.currentScale + ')' );
-                
+
             } ) );
-        
+
     }
-    
+
     ngOnInit() {
-        
+
         this.jsonSchemaService.parseSchema ( this.json );
         this.duration = 750;
-        
+
         // @ts-ignore
-        
+
         this.tree.size ( [ this.height, this.width ] );
         this.tree.nodeSize ( [ 50, 50 ] );
         // @ts-ignore
@@ -87,7 +87,7 @@ export class JsonSchemaComponent implements OnInit {
         .projection ( function( d ) {
             return [ d.y, d.x ];
         } );
-        
+
         // @ts-ignore
         this.svg = d3.select ( 'app-json-schema' ).append ( 'svg' )
         .attr ( 'viewBox', [ 0, 0, this.width, this.height ] )
@@ -95,27 +95,27 @@ export class JsonSchemaComponent implements OnInit {
         .attr ( 'height', this.height + this.margin.top + this.margin.bottom )
         .append ( 'g' )
         .attr ( 'transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')' )
-        
+
         ;
-        
+
         console.log ( '[c]', this.jsonSchemaService.treeData );
-        
+
         this.root = this.jsonSchemaService.treeData[ 0 ];
-        
+
         this.root.x0 = this.height / 2;
         this.root.y0 = 0;
-        
+
         this.update ( this.root );
         // @ts-ignore
         d3.select ( self.frameElement ).style ( 'height', '2500px' );
-        
+
         // Toggle children on click.
-        
+
     }
-    
+
     // define the zoomListener which calls the zoom function on the "zoom" event constrained within the scaleExtents
     // @ts-ignore
-    
+
     click = ( d ) => {
         console.log ( '1 o', Math.abs ( this.position_before.x - this.position_after.x ) < 5 );
         console.log ( '2 o', Math.abs ( this.position_before.y - this.position_after.y ) < 5 );
@@ -125,7 +125,7 @@ export class JsonSchemaComponent implements OnInit {
             this.deltaDragging = { x: this.position_after.x - this.position_before.x, y: this.position_after.y - this.position_before.y };
             return 0;
         }
-        
+
         if ( d.children ) {
             d._children = d.children;
             d.children  = null;
@@ -135,42 +135,41 @@ export class JsonSchemaComponent implements OnInit {
         }
         this.update ( d );
     };
-    
+
     update = source => {
-        
+
         // Compute the new tree layout.
         var i     = 0;
         // @ts-ignore
         var nodes = this.tree.nodes ( this.root ).reverse (),
-        
+
             // @ts-ignore
             links = this.tree.links ( nodes );
-        
+
         // Normalize for fixed-depth.
         nodes.forEach ( function( d ) {
             d.y = d.depth * 240;
         } );
-        
+
         // Update the nodes…
         var node = this.svg.selectAll ( 'g.node' )
         .data ( nodes, function( d: any ) {
             return d.id || (d.id = ++i);
         } );
-        
+
         // Enter any new nodes at the parent's previous position.
         var nodeEnter = node.enter ().append ( 'g' )
         .attr ( 'class', 'node activeDrag' )
         .attr ( 'transform', ( d ) => {
-            
+
             console.log ( '[c] x,y', this.deltaDragging );
             return 'translate(' + source.y0 + this.deltaDragging.y + ',' + source.x0 + this.deltaDragging.x + ')';
         } )
         .on ( 'click', this.click ).on ( 'mousedown', () => {
-            
+
             // @ts-ignore
             console.log ( 'mousedown 0 ', d3.event );
             this.position_before = {
-
 // @ts-ignore
                 x: d3.event.x,
 
@@ -179,13 +178,13 @@ export class JsonSchemaComponent implements OnInit {
             };
             console.log ( '[c] mousedown', this.position_before );
         } );
-        
+
         nodeEnter.append ( 'circle' )
         .attr ( 'r', 1e-6 )
         .style ( 'fill', function( d: any ) {
             return d._children ? 'lightsteelblue' : '#fff';
         } ).style ( 'padding', '20px' )
-        
+
         .call (
             // @ts-ignore
             d3.behavior.drag ().on ( 'drag', ( d: any ) => {
@@ -200,7 +199,7 @@ export class JsonSchemaComponent implements OnInit {
                     d3.transform ( this.svg.attr ( 'transform' ) ).translate[ 1 ] = dy0;
                     // @ts-ignore
                     this.svg.attr ( 'transform', 'translate(' + dy0 + ',' + dx0 + ')scale(' + this.currentScale + ')' );
-                    
+
                 },
             ).on ( 'dragend', () => {
                 // @ts-ignore
@@ -211,7 +210,7 @@ export class JsonSchemaComponent implements OnInit {
                     y: d3.event.sourceEvent.y,
                 };
             } ) );
-        
+
         nodeEnter.append ( 'text' )
         .attr ( 'x', function( d: any ) {
             return d.children || d._children ? -13 : 13;
@@ -238,25 +237,25 @@ export class JsonSchemaComponent implements OnInit {
                 // @ts-ignore
                 this.svg.attr ( 'transform', 'translate(' + dy0 + ',' + dx0 + ')scale(' + this.currentScale + ')' );
             } ) );
-        
+
         // Transition nodes to their new position.
         // @ts-ignore
         var nodeUpdate = node.transition ()
         .duration ( this.duration )
         .attr ( 'transform', ( d: any ) => {
-            
+
             return 'translate(' + d.y + ',' + d.x + ')';
         } );
-        
+
         nodeUpdate.select ( 'circle' )
         .attr ( 'r', 10 )
         .style ( 'fill', function( d: any ) {
             return d._children ? 'lightsteelblue' : '#fff';
         } );
-        
+
         nodeUpdate.select ( 'text' )
         .style ( 'fill-opacity', 1 );
-        
+
         // Transition exiting nodes to the parent's new position.
         // @ts-ignore
         var nodeExit = node.exit ().transition ()
@@ -265,22 +264,22 @@ export class JsonSchemaComponent implements OnInit {
             return 'translate(' + source.y + ',' + source.x + ')';
         } )
         .remove ();
-        
+
         nodeExit.select ( 'circle' )
         .attr ( 'r', 1e-6 );
-        
+
         nodeExit.select ( 'text' )
         .style ( 'fill-opacity', 1e-6 );
-        
+
         // Update the links…
         var link = this.svg.selectAll ( 'path.link' )
         // @ts-ignore
         .data ( links, function( d: any ) {
-                
+
                 return d.target.id;
             },
         );
-        
+
         // Enter any new links at the parent's previous position.
         link.enter ().insert ( 'path', 'g' )
         // .attr ( 'transform', () => {
@@ -298,13 +297,13 @@ export class JsonSchemaComponent implements OnInit {
                 target: o,
             } );
         } );
-        
+
         // Transition links to their new position.
         // @ts-ignore
         link.transition ()
         .duration ( this.duration )
         .attr ( 'd', diagonal );
-        
+
         // Transition exiting nodes to the parent's new position.
         // @ts-ignore
         link.exit ().transition ()
@@ -320,16 +319,16 @@ export class JsonSchemaComponent implements OnInit {
             } );
         } )
         .remove ();
-        
+
         // Stash the old positions for transition.
         nodes.forEach ( function( d ) {
             d.x0 = d.x;
             d.y0 = d.y;
         } );
-        
+
         // this.deltaDragging =  {x: 0, y: 0}
     };
-    
+
 }
 
 
